9b77d7d54d455b7c97dcff0b849c923a01ec9763
shibbolithic <akshitashar012@gmail.com>
2025-02-07 16:05:24 +0530

    search integrated!

---
+++ PakhisAthlinix/create/team/teamScreen/ViewTeamMemberCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  ViewTeamMemberCollectionViewCell.swift
+//  PakhisAthlinix
+//
+//  Created by admin65 on 07/02/25.
+//
+
+import UIKit
+
+class ViewTeamMemberCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ PakhisAthlinix/explore/SearchTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  SearchTableViewCell.swift
+//  PakhisAthlinix
+//
+//  Created by admin65 on 07/02/25.
+//
+
+import UIKit
+
+class SearchTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
